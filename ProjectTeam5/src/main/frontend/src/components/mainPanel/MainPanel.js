810effa941b2d9e1214ee240cdc3f305ceee8751
Zionoi <rladygks2848@naver.com>
2024-09-09 14:45:52 +0900

    로그인오류 수정 완료

--- ProjectTeam5/src/main/frontend/src/components/mainPanel/MainPanel.js
+++ ProjectTeam5/src/main/frontend/src/components/mainPanel/MainPanel.js
@@ -11,42 +11,22 @@ import CommentsSection from '../home/CommentsSection.jsx';
 import TopCommnets from './TopComments.jsx';
 
 import { useEffect, useState } from 'react';
-import Login from '../login/Login.jsx';
-import SignUpPage from '../SignUpPage/SignUpPage.jsx';
 
 // import MusicPlayer from './MusicPlayer.jsx';
 
-function MainPanel() { // onLogout props 추가
-    const navigate = useNavigate();
+function MainPanel({onLogout}) { // onLogout props 추가
+  const navigate = useNavigate();
   
-
-    // const handleLogout = () => {
-    //   onLogout(); // 부모 컴포넌트의 로그아웃 핸들러 호출
-    //   navigate('/login'); // 로그인 페이지로 리다이렉트
-    // };
-
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    useEffect(() => {
-      // 로컬 스토리지에서 로그인 상태 확인
-      const token = localStorage.getItem('token');
-      const id = localStorage.getItem('id');
-      if (token && id) {
-        
-        setIsLoggedIn(true);
-      }
-    }, []);
 
     const handleLogout = () => {
-    
-      setIsLoggedIn(false); // 로그인 상태를 false로 설정
-      navigate('/login');
-      localStorage.clear(); // 로컬 스토리지 클리어
+      onLogout(); // 부모 컴포넌트의 로그아웃 핸들러 호출
+      navigate('/login'); // 로그인 페이지로 리다이렉트
     };
+
+    
   
     return (
-      <div>
-      {isLoggedIn ? 
+      
 
       <div className="app" style={{ display: 'flex' }}>
         <div className="left-panel">
@@ -62,14 +42,6 @@ function MainPanel() { // onLogout props 추가
         <FriendsSection />
       </aside>
     </div>
-      : 
-      <div>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/SignUpPage" element={<SignUpPage />}/>
-        </Routes>
-      </div>}
-      </div>
     );
   }
   
