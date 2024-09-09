0d2c70cd3f182f7a7ccb02bb6ce21354230a0285
iseul011 <kayira011@naver.com>
2024-09-09 14:23:30 +0900

    message(springboot)

--- ProjectTeam5/src/main/java/com/study/spring/controller/MessageController.java
+++ ProjectTeam5/src/main/java/com/study/spring/controller/MessageController.java
@@ -1,5 +1,7 @@
 package com.study.spring.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +20,33 @@ public class MessageController {
     // 메시지 전송 API
     @PostMapping("/send")
     public Message sendMessage(@RequestBody Message message) {
-    	System.out.println("컨컨컨컨컨컨컨컨컨컨컨컨컨컨컨"+message);
-
-    	return messageService.sendMessage(message);
+    	
+        return messageService.sendMessage(message);
     }
 
     // 특정 사용자가 받은 메시지 조회 API
     @GetMapping("/received/{friendId}")
     public List<Message> getReceivedMessages(@PathVariable String friendId) {
         return messageService.getReceivedMessages(friendId);
+    }
+    
+    // 특정 메시지 조회 API
+    @GetMapping("/detail/{m_num}")
+    public Message getMessageDetail(@PathVariable("m_num") Long mNum) {
+        return messageService.getMessageById(mNum);
+    }
+
+    // 메시지 읽음 처리 API
+    @PostMapping("/read/{mNum}")
+    public void markMessageAsRead(@PathVariable Long mNum) {
+    	System.out.println("컨컨컨컨컨컨 메시지 읽음 : "  + mNum);
+        messageService.markMessageAsRead(mNum);
+    }
+
+    // 메시지 삭제 API
+    @DeleteMapping("/delete/{mNum}")
+    public void deleteMessage(@PathVariable String mNum) {
+    	System.out.println("컨컨컨컨컨컨 메시지 삭제 : "  + mNum);
+        messageService.deleteMessage(Long.valueOf(mNum));
     }
 }
--- ProjectTeam5/src/main/java/com/study/spring/repository/MessageRepository.java
+++ ProjectTeam5/src/main/java/com/study/spring/repository/MessageRepository.java
@@ -2,13 +2,25 @@ package com.study.spring.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Sort;
 
 import com.study.spring.domain.Message;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-	
-    // 특정 사용자가 받은 모든 메시지 조회
-    List<Message> findByFriendId(String friendId);
+
+    // 특정 사용자가 받은 모든 메시지 조회 (정렬)
+    List<Message> findByFriendId(String friendId, Sort sort);
+
+    // 메시지 읽음 처리
+    @Modifying
+    @Query("UPDATE Message m SET m.isReading = 0 WHERE m.mNum = :mNum")
+    void markMessageAsRead(@Param("mNum") Long mNum);
+
+    // 메시지 삭제
+    void deleteById(Long mNum);
 }
