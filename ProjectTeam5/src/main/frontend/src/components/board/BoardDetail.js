dcdb58c760d6dd37b7b3b51aefa27030f62f79ad
ebe0025 <tjdgns3728@naver.com>
2024-09-13 13:00:35 +0900

    포춘 기능 방명록 기능 구현
    포춘 기능 방명록 기능 구현

--- ProjectTeam5/src/main/frontend/src/components/board/BoardDetail.js
+++ ProjectTeam5/src/main/frontend/src/components/board/BoardDetail.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Slider from "react-slick";
-
 
 // npm install react-slick slick-carousel 
 // 인설트 해야함
@@ -14,6 +13,7 @@ import Slider from "react-slick";
 function BoardDetail() {
   const { bNum } = useParams();  // URL 파라미터에서 bnum을 가져옵니다.
   const [detail, setDetail] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get('/board/detail', { params: { bNum } })  // bnum을 올바르게 전달
@@ -27,6 +27,24 @@ function BoardDetail() {
         alert("상세 정보를 가져오는 중 오류가 발생했습니다.");
       });
   }, [bNum]);  // bnum이 변경될 때마다 이펙트가 재실행되도록 의존성 배열에 추가
+
+  const handleDelete = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      axios.delete(`/board/delete/${bNum}`)
+        .then((response) => {
+          alert("게시물이 삭제되었습니다.");
+          navigate(`/home`);  // 삭제 후 메인 페이지로 이동
+        })
+        .catch((error) => {
+          console.error("Error deleting post:", error);
+          alert("게시물 삭제 중 오류가 발생했습니다.");
+        });
+    }
+  };
+
+  const handleEdit = () => {
+    navigate(`/boardEdit/${bNum}`);  // 수정 페이지로 이동
+  };
 
   if (!detail) {
     return <div>Loading...</div>;  // 데이터를 가져오는 중일 때 표시되는 로딩 메시지
@@ -58,6 +76,12 @@ function BoardDetail() {
       </Slider>
       <p>{detail.bcontent}</p>
       <p>{detail.bNum}</p>
+
+      {/* 수정 및 삭제 버튼 추가 */}
+      <div>
+        <button onClick={handleEdit}>수정하기</button>
+        <button onClick={handleDelete}>삭제하기</button>
+      </div>
     </div>
   );
 }
--- ProjectTeam5/src/main/java/com/study/spring/service/GuestbookService.java
+++ ProjectTeam5/src/main/java/com/study/spring/service/GuestbookService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.study.spring.domain.Guestbook;
@@ -18,10 +21,12 @@ public class GuestbookService {
 		guestbookRepository.save(guestbook);
 	}
 
-	public List<Guestbook> totalGuestbook() {
-	
-		return guestbookRepository.findAll();
+    // 특정 페이지의 방명록을 페이징만큼 가져옴
+	public Page<Guestbook> getGuestbookPage(int page, int size, String memId) {
+	    Pageable pageable = PageRequest.of(page, size);
+	    return guestbookRepository.findByMemIdOrderByCreateDateDesc(memId, pageable);
 	}
+
 
 	public Optional<Guestbook> detailGuestbook(Long gNum) {
 		
