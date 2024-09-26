a9af1bf27b497015951d293cb94049bb83da2de5
Zionoi <163436710+Zionoi@users.noreply.github.com>
2024-09-27 01:10:35 +0900

    .

--- hometree5/src/main/frontend/config-overrides.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    webpack: function (config, env) {
-      config.output.filename = 'static/js/[name].[hash:8].js';
-      config.output.chunkFilename = 'static/js/[name].[hash:8].chunk.js';
-      return config;
-    },
-  };
